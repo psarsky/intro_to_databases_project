bd9687773d27e44fc902223b664f56481cc835bc
Jakub Psarski <psarski@student.agh.edu.pl>
2024-12-10 02:44:12 +0100

    new diagram and source code

---
+++ source_code/foreign_keys.sql
@@ -0,0 +1,293 @@
+-- Reference: ClassAttendance_Classes (table: ClassAttendance)
+ALTER TABLE ClassAttendance
+    ADD CONSTRAINT ClassAttendance_Classes
+        FOREIGN KEY (ClassID)
+            REFERENCES Classes (ClassID);
+
+-- Reference: ClassAttendance_Users (table: ClassAttendance)
+ALTER TABLE ClassAttendance
+    ADD CONSTRAINT ClassAttendance_Users
+        FOREIGN KEY (UserID)
+            REFERENCES Users (UserID);
+
+-- Reference: Classes_Employees (table: Classes)
+ALTER TABLE Classes
+    ADD CONSTRAINT Classes_Employees
+        FOREIGN KEY (TeacherID)
+            REFERENCES Employees (EmployeeID);
+
+-- Reference: Classes_OnlineAsynchronousClasses (table: OnlineAsynchronousClasses)
+ALTER TABLE OnlineAsynchronousClasses
+    ADD CONSTRAINT Classes_OnlineAsynchronousClasses
+        FOREIGN KEY (ClassID)
+            REFERENCES Classes (ClassID);
+
+-- Reference: Classes_OnlineSynchronousClasses (table: OnlineSynchronousClasses)
+ALTER TABLE OnlineSynchronousClasses
+    ADD CONSTRAINT Classes_OnlineSynchronousClasses
+        FOREIGN KEY (ClassID)
+            REFERENCES Classes (ClassID);
+
+-- Reference: Classes_StationaryClasses (table: StationaryClasses)
+ALTER TABLE StationaryClasses
+    ADD CONSTRAINT Classes_StationaryClasses
+        FOREIGN KEY (ClassID)
+            REFERENCES Classes (ClassID);
+
+-- Reference: Classes_StudyMeetings (table: Classes)
+ALTER TABLE Classes
+    ADD CONSTRAINT Classes_StudyMeetings
+        FOREIGN KEY (MeetingID)
+            REFERENCES StudyMeetings (MeetingID);
+
+-- Reference: Classes_SubjectDetails (table: Classes)
+ALTER TABLE Classes
+    ADD CONSTRAINT Classes_SubjectDetails
+        FOREIGN KEY (StudyID, SubjectID)
+            REFERENCES SubjectDetails (StudyID, SubjectID);
+
+-- Reference: CourseMeetingAttendance_CourseMeetings (table: CourseMeetingAttendance)
+ALTER TABLE CourseMeetingAttendance
+    ADD CONSTRAINT CourseMeetingAttendance_CourseMeetings
+        FOREIGN KEY (MeetingID)
+            REFERENCES CourseMeetings (MeetingID);
+
+-- Reference: CourseMeetingAttendance_Users (table: CourseMeetingAttendance)
+ALTER TABLE CourseMeetingAttendance
+    ADD CONSTRAINT CourseMeetingAttendance_Users
+        FOREIGN KEY (UserID)
+            REFERENCES Users (UserID);
+
+-- Reference: CourseMeetings_CourseModules (table: CourseMeetings)
+ALTER TABLE CourseMeetings
+    ADD CONSTRAINT CourseMeetings_CourseModules
+        FOREIGN KEY (ModuleID)
+            REFERENCES CourseModules (ModuleID);
+
+-- Reference: CourseMeetings_Employees (table: CourseMeetings)
+ALTER TABLE CourseMeetings
+    ADD CONSTRAINT CourseMeetings_Employees
+        FOREIGN KEY (TeacherID)
+            REFERENCES Employees (EmployeeID);
+
+-- Reference: CourseMeetings_OnlineAsynchronousCourseMeetings (table: CourseMeetings)
+ALTER TABLE CourseMeetings
+    ADD CONSTRAINT CourseMeetings_OnlineAsynchronousCourseMeetings
+        FOREIGN KEY (MeetingID)
+            REFERENCES OnlineAsynchronousCourseMeetings (MeetingID);
+
+-- Reference: CourseMeetings_OnlineSynchronousCourseMeetings (table: CourseMeetings)
+ALTER TABLE CourseMeetings
+    ADD CONSTRAINT CourseMeetings_OnlineSynchronousCourseMeetings
+        FOREIGN KEY (MeetingID)
+            REFERENCES OnlineSynchronousCourseMeetings (MeetingID);
+
+-- Reference: CourseMeetings_StationaryCourseMeetings (table: CourseMeetings)
+ALTER TABLE CourseMeetings
+    ADD CONSTRAINT CourseMeetings_StationaryCourseMeetings
+        FOREIGN KEY (MeetingID)
+            REFERENCES StationaryCourseMeetings (MeetingID);
+
+-- Reference: CourseMeetings_TranslatorLanguages (table: CourseMeetings)
+ALTER TABLE CourseMeetings
+    ADD CONSTRAINT CourseMeetings_TranslatorLanguages
+        FOREIGN KEY (TranslatorID, LanguageID)
+            REFERENCES TranslatorLanguages (TranslatorID, LanguageID);
+
+-- Reference: CourseModules_Courses (table: CourseModules)
+ALTER TABLE CourseModules
+    ADD CONSTRAINT CourseModules_Courses
+        FOREIGN KEY (CourseID)
+            REFERENCES Courses (CourseID);
+
+-- Reference: CourseOrders_Courses (table: CourseOrders)
+ALTER TABLE CourseOrders
+    ADD CONSTRAINT CourseOrders_Courses
+        FOREIGN KEY (CourseID)
+            REFERENCES Courses (CourseID);
+
+-- Reference: CourseOrders_Orders (table: CourseOrders)
+ALTER TABLE CourseOrders
+    ADD CONSTRAINT CourseOrders_Orders
+        FOREIGN KEY (OrderID)
+            REFERENCES Orders (OrderID);
+
+-- Reference: Courses_Employees (table: Courses)
+ALTER TABLE Courses
+    ADD CONSTRAINT Courses_Employees
+        FOREIGN KEY (CoordinatorID)
+            REFERENCES Employees (EmployeeID);
+
+-- Reference: Employees_Positions (table: Employees)
+ALTER TABLE Employees
+    ADD CONSTRAINT Employees_Positions
+        FOREIGN KEY (PositionID)
+            REFERENCES Positions (PositionID);
+
+-- Reference: InternshipAttendance_StudentLists (table: InternshipAttendance)
+ALTER TABLE InternshipAttendance
+    ADD CONSTRAINT InternshipAttendance_StudentLists
+        FOREIGN KEY (StudyID, UserID)
+            REFERENCES StudentLists (StudyID, UserID);
+
+-- Reference: InternshipDetails_Internships (table: InternshipAttendance)
+ALTER TABLE InternshipAttendance
+    ADD CONSTRAINT InternshipDetails_Internships
+        FOREIGN KEY (InternshipID)
+            REFERENCES Internships (InternshipID);
+
+-- Reference: Internships_Employees (table: Internships)
+ALTER TABLE Internships
+    ADD CONSTRAINT Internships_Employees
+        FOREIGN KEY (TeacherID)
+            REFERENCES Employees (EmployeeID);
+
+-- Reference: Internships_Studies (table: Internships)
+ALTER TABLE Internships
+    ADD CONSTRAINT Internships_Studies
+        FOREIGN KEY (StudyID)
+            REFERENCES Studies (StudyID);
+
+-- Reference: Meetings_TranslatorLanguages (table: Classes)
+ALTER TABLE Classes
+    ADD CONSTRAINT Meetings_TranslatorLanguages
+        FOREIGN KEY (TranslatorID, LanguageID)
+            REFERENCES TranslatorLanguages (TranslatorID, LanguageID);
+
+-- Reference: Orders_Users (table: Orders)
+ALTER TABLE Orders
+    ADD CONSTRAINT Orders_Users
+        FOREIGN KEY (UserID)
+            REFERENCES Users (UserID);
+
+-- Reference: Reservations_Rooms (table: Reservations)
+ALTER TABLE Reservations
+    ADD CONSTRAINT Reservations_Rooms
+        FOREIGN KEY (RoomID)
+            REFERENCES Rooms (RoomID);
+
+-- Reference: StationaryClasses_Reservations (table: StationaryClasses)
+ALTER TABLE StationaryClasses
+    ADD CONSTRAINT StationaryClasses_Reservations
+        FOREIGN KEY (ReservationID)
+            REFERENCES Reservations (ReservationID);
+
+-- Reference: StationaryCourseMeetings_Reservations (table: StationaryCourseMeetings)
+ALTER TABLE StationaryCourseMeetings
+    ADD CONSTRAINT StationaryCourseMeetings_Reservations
+        FOREIGN KEY (ReservationID)
+            REFERENCES Reservations (ReservationID);
+
+-- Reference: StudentLists_Studies (table: StudentLists)
+ALTER TABLE StudentLists
+    ADD CONSTRAINT StudentLists_Studies
+        FOREIGN KEY (StudyID)
+            REFERENCES Studies (StudyID);
+
+-- Reference: StudentLists_StudyGrades (table: StudyGrades)
+ALTER TABLE StudyGrades
+    ADD CONSTRAINT StudentLists_StudyGrades
+        FOREIGN KEY (StudyID, UserID)
+            REFERENCES StudentLists (StudyID, UserID);
+
+-- Reference: StudentLists_Users (table: StudentLists)
+ALTER TABLE StudentLists
+    ADD CONSTRAINT StudentLists_Users
+        FOREIGN KEY (UserID)
+            REFERENCES Users (UserID);
+
+-- Reference: Studies_Employees (table: Studies)
+ALTER TABLE Studies
+    ADD CONSTRAINT Studies_Employees
+        FOREIGN KEY (CoordinatorID)
+            REFERENCES Employees (EmployeeID);
+
+-- Reference: StudyMeetingOrders_Orders (table: StudyMeetingOrders)
+ALTER TABLE StudyMeetingOrders
+    ADD CONSTRAINT StudyMeetingOrders_Orders
+        FOREIGN KEY (OrderID)
+            REFERENCES Orders (OrderID);
+
+-- Reference: StudyMeetingOrders_StudyMeetings (table: StudyMeetingOrders)
+ALTER TABLE StudyMeetingOrders
+    ADD CONSTRAINT StudyMeetingOrders_StudyMeetings
+        FOREIGN KEY (MeetingID)
+            REFERENCES StudyMeetings (MeetingID);
+
+-- Reference: StudyOrders_Orders (table: StudyOrders)
+ALTER TABLE StudyOrders
+    ADD CONSTRAINT StudyOrders_Orders
+        FOREIGN KEY (OrderID)
+            REFERENCES Orders (OrderID);
+
+-- Reference: StudyOrders_Studies (table: StudyOrders)
+ALTER TABLE StudyOrders
+    ADD CONSTRAINT StudyOrders_Studies
+        FOREIGN KEY (StudyID)
+            REFERENCES Studies (StudyID);
+
+-- Reference: SubjectDetails_Studies (table: SubjectDetails)
+ALTER TABLE SubjectDetails
+    ADD CONSTRAINT SubjectDetails_Studies
+        FOREIGN KEY (StudyID)
+            REFERENCES Studies (StudyID);
+
+-- Reference: SubjectDetails_Subjects (table: SubjectDetails)
+ALTER TABLE SubjectDetails
+    ADD CONSTRAINT SubjectDetails_Subjects
+        FOREIGN KEY (SubjectID)
+            REFERENCES Subjects (SubjectID);
+
+-- Reference: SubjectGrades_StudentLists (table: SubjectGrades)
+ALTER TABLE SubjectGrades
+    ADD CONSTRAINT SubjectGrades_StudentLists
+        FOREIGN KEY (StudyID, UserID)
+            REFERENCES StudentLists (StudyID, UserID);
+
+-- Reference: SubjectGrades_Subjects (table: SubjectGrades)
+ALTER TABLE SubjectGrades
+    ADD CONSTRAINT SubjectGrades_Subjects
+        FOREIGN KEY (SubjectID)
+            REFERENCES Subjects (SubjectID);
+
+-- Reference: Subjects_Employees (table: Subjects)
+ALTER TABLE Subjects
+    ADD CONSTRAINT Subjects_Employees
+        FOREIGN KEY (CoordinatorID)
+            REFERENCES Employees (EmployeeID);
+
+-- Reference: TranslatorLanguages_Employees (table: TranslatorLanguages)
+ALTER TABLE TranslatorLanguages
+    ADD CONSTRAINT TranslatorLanguages_Employees
+        FOREIGN KEY (TranslatorID)
+            REFERENCES Employees (EmployeeID);
+
+-- Reference: TranslatorLanguages_Languages (table: TranslatorLanguages)
+ALTER TABLE TranslatorLanguages
+    ADD CONSTRAINT TranslatorLanguages_Languages
+        FOREIGN KEY (LanguageID)
+            REFERENCES Languages (LanguageID);
+
+-- Reference: WebinarOrders_Orders (table: WebinarOrders)
+ALTER TABLE WebinarOrders
+    ADD CONSTRAINT WebinarOrders_Orders
+        FOREIGN KEY (OrderID)
+            REFERENCES Orders (OrderID);
+
+-- Reference: WebinarOrders_Webinars (table: WebinarOrders)
+ALTER TABLE WebinarOrders
+    ADD CONSTRAINT WebinarOrders_Webinars
+        FOREIGN KEY (WebinarID)
+            REFERENCES Webinars (WebinarID);
+
+-- Reference: Webinars_Employees (table: Webinars)
+ALTER TABLE Webinars
+    ADD CONSTRAINT Webinars_Employees
+        FOREIGN KEY (TeacherID)
+            REFERENCES Employees (EmployeeID);
+
+-- Reference: Webinars_TranslatorLanguages (table: Webinars)
+ALTER TABLE Webinars
+    ADD CONSTRAINT Webinars_TranslatorLanguages
+        FOREIGN KEY (TranslatorID, LanguageID)
+            REFERENCES TranslatorLanguages (TranslatorID, LanguageID);
---
+++ source_code/tables.sql
@@ -0,0 +1,375 @@
+-- PEOPLE
+
+-- Table: Users
+CREATE TABLE Users
+(
+    UserID       int          NOT NULL IDENTITY,
+    FirstName    nvarchar(30) NOT NULL,
+    LastName     nvarchar(30) NOT NULL,
+    Address      nvarchar(30) NOT NULL,
+    PostalCode   nvarchar(6)  NOT NULL,
+    City         nvarchar(50) NOT NULL,
+    Country      nvarchar(20) NOT NULL,
+    RegisterDate datetime     NOT NULL,
+    Email        nvarchar(50) NOT NULL,
+    Phone        nvarchar(15) NULL,
+    CONSTRAINT Users_pk PRIMARY KEY (UserID)
+);
+
+-- Table: Employees
+CREATE TABLE Employees
+(
+    EmployeeID int          NOT NULL IDENTITY,
+    PositionID int          NOT NULL,
+    FirstName  nvarchar(30) NOT NULL,
+    LastName   nvarchar(30) NOT NULL,
+    HireDate   datetime     NOT NULL,
+    Email      nvarchar(50) NOT NULL,
+    Phone      nvarchar(15) NOT NULL,
+    CONSTRAINT Employees_pk PRIMARY KEY (EmployeeID)
+);
+
+-- Table: Positions
+CREATE TABLE Positions
+(
+    PositionID int          NOT NULL IDENTITY,
+    Name       nvarchar(20) NOT NULL,
+    CONSTRAINT Positions_pk PRIMARY KEY (PositionID)
+);
+
+-- Table: Languages
+CREATE TABLE Languages
+(
+    LanguageID   int          NOT NULL IDENTITY,
+    LanguageName nvarchar(15) NOT NULL,
+    CONSTRAINT Languages_pk PRIMARY KEY (LanguageID)
+);
+
+-- Table: TranslatorLanguages
+CREATE TABLE TranslatorLanguages
+(
+    TranslatorID int NOT NULL,
+    LanguageID   int NOT NULL,
+    CONSTRAINT TranslatorLanguages_pk PRIMARY KEY (TranslatorID, LanguageID)
+);
+
+
+
+-- WEBINARS
+
+-- Table: Webinars
+CREATE TABLE Webinars
+(
+    WebinarID    int           NOT NULL IDENTITY,
+    TeacherID    int           NOT NULL,
+    LanguageID   int           NULL,
+    TranslatorID int           NULL,
+    Title        nvarchar(100) NOT NULL,
+    Description  nvarchar(max) NULL,
+    Date         datetime      NOT NULL,
+    Duration     time          NOT NULL,
+    MeetingLink  nvarchar(100) NOT NULL,
+    VideoLink    nvarchar(100) NOT NULL,
+    Price        money         NULL,
+    CONSTRAINT Webinars_pk PRIMARY KEY (WebinarID)
+);
+
+
+
+-- COURSES
+
+-- Table: Courses
+CREATE TABLE Courses
+(
+    CourseID      int           NOT NULL IDENTITY,
+    CoordinatorID int           NOT NULL,
+    Title         nvarchar(100) NOT NULL,
+    Description   nvarchar(max) NULL,
+    Price         money         NOT NULL,
+    CONSTRAINT Courses_pk PRIMARY KEY (CourseID)
+);
+
+-- Table: CourseModules
+CREATE TABLE CourseModules
+(
+    ModuleID    int           NOT NULL IDENTITY,
+    CourseID    int           NOT NULL,
+    Title       nvarchar(100) NOT NULL,
+    Description nvarchar(max) NULL,
+    ModuleType  nvarchar(12)  NOT NULL,
+    CONSTRAINT CourseModules_pk PRIMARY KEY (ModuleID)
+);
+
+-- Table: CourseMeetings
+CREATE TABLE CourseMeetings
+(
+    MeetingID    int           NOT NULL IDENTITY,
+    ModuleID     int           NOT NULL,
+    TeacherID    int           NOT NULL,
+    LanguageID   int           NULL,
+    TranslatorID int           NULL,
+    Title        nvarchar(100) NOT NULL,
+    Description  nvarchar(max) NOT NULL,
+    Date         datetime      NOT NULL,
+    Duration     time          NOT NULL,
+    CONSTRAINT CourseMeetings_pk PRIMARY KEY (MeetingID)
+);
+
+-- Table: CourseMeetingAttendance
+CREATE TABLE CourseMeetingAttendance
+(
+    MeetingID int NOT NULL,
+    UserID    int NOT NULL,
+    Attended  bit NOT NULL DEFAULT 0,
+    CONSTRAINT CourseMeetingAttendance_pk PRIMARY KEY (UserID, MeetingID)
+);
+
+-- Table: OnlineAsynchronousCourseMeetings
+CREATE TABLE OnlineAsynchronousCourseMeetings
+(
+    MeetingID int           NOT NULL,
+    VideoLink nvarchar(100) NOT NULL,
+    CONSTRAINT OnlineAsynchronousCourseMeetings_pk PRIMARY KEY (MeetingID)
+);
+
+-- Table: OnlineSynchronousCourseMeetings
+CREATE TABLE OnlineSynchronousCourseMeetings
+(
+    MeetingID   int           NOT NULL,
+    MeetingLink nvarchar(100) NOT NULL,
+    VideoLink   nvarchar(100) NOT NULL,
+    CONSTRAINT OnlineSynchronousCourseMeetings_pk PRIMARY KEY (MeetingID)
+);
+
+-- Table: StationaryCourseMeetings
+CREATE TABLE StationaryCourseMeetings
+(
+    MeetingID     int NOT NULL,
+    ReservationID int NOT NULL,
+    Limit         int NOT NULL,
+    CONSTRAINT StationaryCourseMeetings_pk PRIMARY KEY (MeetingID)
+);
+
+
+
+-- STUDIES
+
+-- Table: Studies
+CREATE TABLE Studies
+(
+    StudyID       int           NOT NULL IDENTITY,
+    CoordinatorID int           NOT NULL,
+    Title         nvarchar(100) NOT NULL,
+    Description   nvarchar(max) NULL,
+    TuitionFee    money         NOT NULL,
+    CONSTRAINT Studies_pk PRIMARY KEY (StudyID)
+);
+
+-- Table: StudentLists
+CREATE TABLE StudentLists
+(
+    StudyID int NOT NULL,
+    UserID  int NOT NULL,
+    CONSTRAINT StudentLists_pk PRIMARY KEY (StudyID, UserID)
+);
+
+-- Table: StudyGrades
+CREATE TABLE StudyGrades
+(
+    StudyID int  NOT NULL,
+    UserID  int  NOT NULL,
+    Grade   real NOT NULL,
+    CONSTRAINT StudyGrades_pk PRIMARY KEY (StudyID, UserID)
+);
+
+-- Table: Subjects
+CREATE TABLE Subjects
+(
+    SubjectID     int           NOT NULL IDENTITY,
+    CoordinatorID int           NOT NULL,
+    Title         nvarchar(100) NOT NULL,
+    Description   nvarchar(max) NULL,
+    CONSTRAINT Subjects_pk PRIMARY KEY (SubjectID)
+);
+
+-- Table: SubjectDetails
+CREATE TABLE SubjectDetails
+(
+    StudyID    int NOT NULL,
+    SubjectID  int NOT NULL,
+    SemesterNo int NOT NULL,
+    CONSTRAINT SubjectDetails_pk PRIMARY KEY (StudyID, SubjectID)
+);
+
+-- Table: SubjectGrades
+CREATE TABLE SubjectGrades
+(
+    StudyID   int  NOT NULL,
+    UserID    int  NOT NULL,
+    SubjectID int  NOT NULL,
+    Grade     real NOT NULL,
+    CONSTRAINT SubjectGrades_pk PRIMARY KEY (SubjectID, UserID, StudyID)
+);
+
+-- Table: Internships
+CREATE TABLE Internships
+(
+    InternshipID int      NOT NULL IDENTITY,
+    StudyID      int      NOT NULL,
+    TeacherID    int      NOT NULL,
+    StartDate    datetime NOT NULL,
+    EndDate      datetime NOT NULL,
+    CONSTRAINT Internships_pk PRIMARY KEY (InternshipID)
+);
+
+-- Table: InternshipAttendance
+CREATE TABLE InternshipAttendance
+(
+    InternshipID int NOT NULL,
+    UserID       int NOT NULL,
+    StudyID      int NOT NULL,
+    Attended     bit NOT NULL,
+    CONSTRAINT InternshipAttendance_pk PRIMARY KEY (InternshipID, UserID)
+);
+
+
+
+-- MEETINGS
+
+-- Table: Classes
+CREATE TABLE Classes
+(
+    ClassID      int           NOT NULL IDENTITY,
+    StudyID      int           NOT NULL,
+    SubjectID    int           NOT NULL,
+    MeetingID    int           NULL,
+    TeacherID    int           NOT NULL,
+    LanguageID   int           NULL,
+    TranslatorID int           NULL,
+    Title        nvarchar(100) NOT NULL,
+    Description  nvarchar(max) NULL,
+    Date         datetime      NOT NULL,
+    Duration     time          NOT NULL,
+    Price        money         NOT NULL,
+    CONSTRAINT Classes_pk PRIMARY KEY (ClassID)
+);
+
+-- Table: ClassAttendance
+CREATE TABLE ClassAttendance
+(
+    ClassID  int NOT NULL,
+    UserID   int NOT NULL,
+    Attended bit NOT NULL,
+    CONSTRAINT MeetingDetails_pk PRIMARY KEY (ClassID, UserID)
+);
+
+-- Table: StudyMeetings
+CREATE TABLE StudyMeetings
+(
+    MeetingID int      NOT NULL IDENTITY,
+    BeginDate datetime NOT NULL,
+    EndDate   datetime NOT NULL,
+    Price     money    NOT NULL,
+    Limit     int      NOT NULL,
+    CONSTRAINT StudyMeetings_pk PRIMARY KEY (MeetingID)
+);
+
+-- Table: StationaryClasses
+CREATE TABLE StationaryClasses
+(
+    ClassID       int NOT NULL,
+    MeetingID     int NOT NULL,
+    ReservationID int NOT NULL,
+    CONSTRAINT StationaryClasses_pk PRIMARY KEY (ClassID)
+);
+
+-- Table: OnlineAsynchronousClasses
+CREATE TABLE OnlineAsynchronousClasses
+(
+    ClassID   int           NOT NULL,
+    VideoLink nvarchar(100) NOT NULL,
+    CONSTRAINT OnlineAsynchronousClasses_pk PRIMARY KEY (ClassID)
+);
+
+-- Table: OnlineSynchronousClasses
+CREATE TABLE OnlineSynchronousClasses
+(
+    ClassID     int           NOT NULL,
+    MeetingLink nvarchar(100) NOT NULL,
+    VideoLink   nvarchar(100) NOT NULL,
+    CONSTRAINT OnlineSynchronousClasses_pk PRIMARY KEY (ClassID)
+);
+
+
+
+-- ROOMS
+
+-- Table: Rooms
+CREATE TABLE Rooms
+(
+    RoomID     int           NOT NULL IDENTITY,
+    RoomNumber int           NOT NULL,
+    Location   nvarchar(max) NOT NULL,
+    Limit      int           NOT NULL,
+    CONSTRAINT Rooms_pk PRIMARY KEY (RoomID)
+);
+
+-- Table: Reservations
+CREATE TABLE Reservations
+(
+    ReservationID int      NOT NULL IDENTITY,
+    RoomID        int      NOT NULL,
+    StartTime     datetime NOT NULL,
+    EndTime       datetime NOT NULL,
+    CONSTRAINT Reservations_pk PRIMARY KEY (ReservationID)
+);
+
+
+
+-- ORDERS
+
+-- Table: Orders
+CREATE TABLE Orders
+(
+    OrderID     int      NOT NULL IDENTITY,
+    UserID      int      NOT NULL,
+    OrderDate   datetime NOT NULL,
+    PaymentDate datetime NULL,
+    CONSTRAINT Orders_pk PRIMARY KEY (OrderID)
+);
+
+-- Table: WebinarOrders
+CREATE TABLE WebinarOrders
+(
+    WebinarID int   NOT NULL,
+    Price     money NOT NULL,
+    OrderID   int   NOT NULL,
+    CONSTRAINT WebinarOrders_pk PRIMARY KEY (WebinarID)
+);
+
+-- Table: CourseOrders
+CREATE TABLE CourseOrders
+(
+    OrderID  int   NOT NULL,
+    CourseID int   NOT NULL,
+    Price    money NOT NULL,
+    CONSTRAINT CourseOrders_pk PRIMARY KEY (OrderID, CourseID)
+);
+
+-- Table: StudyOrders
+CREATE TABLE StudyOrders
+(
+    OrderID int   NOT NULL,
+    StudyID int   NOT NULL,
+    Price   money NOT NULL,
+    CONSTRAINT StudyOrders_pk PRIMARY KEY (OrderID, StudyID)
+);
+
+-- Table: StudyMeetingOrders
+CREATE TABLE StudyMeetingOrders
+(
+    OrderID   int   NOT NULL,
+    MeetingID int   NOT NULL,
+    Price     money NOT NULL,
+    CONSTRAINT StudyMeetingOrders_pk PRIMARY KEY (OrderID, MeetingID)
+);
